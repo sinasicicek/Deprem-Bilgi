14f69cb247519befa13be750bd348ee93a883f71
sinasicicek <56018837+sinasicicek@users.noreply.github.com>
2021-10-23 10:51:16 +0000

    Kac Adet  deprem cekilecegi filtreleniyor

--- root.js
+++ root.js
@@ -1,6 +1,7 @@
 const conta=document.querySelector(".container")
-async function getir() {
-    const geti=await fetch("https://api.orhanaydogdu.com.tr/deprem/live.php?limit=20");
+
+async function getir(adet=20) {
+    const geti=await fetch(`https://api.orhanaydogdu.com.tr/deprem/live.php?limit=${adet}`);
     return await geti.json()
 }
 
@@ -12,16 +13,14 @@ function obj_build(...params) {
     let siddet_no=Number(params[2])
     let sinif=["items","brdr"]
         div.classList.add(...sinif)
-
-
-  
-
 let str=Created("h3")
     str.textContent=params[1];
 
 let h5=Created("h5");
         h5.textContent=params[0];
-
+        if (siddet_no >4 && siddet_no <9){
+            Noti(siddet_no,params[1],params[0])
+        }
        
 let span=Created("span")
    if (siddet_no >4) {
@@ -39,15 +38,15 @@ let eklenecek=[str,h5,span]
 return div;
 }
 
-function basla() {
-    let bc=getir();
+function basla(kac_adet=20) {
+    let bc=getir(kac_adet);
     bc.then( geldi=>{
    for (const key in geldi) {
       
        if (Object.hasOwnProperty.call(geldi, key)) 
        {
            const element = geldi[key];
-           console.log(geldi[key].length);
+           console.log("basla ıcınde"+geldi[key].length);
          
             for (let index = 0; index < element.length; index++) {
                 const eleman = element[index];
@@ -85,4 +84,42 @@ buton.addEventListener("click",()=>{
  
  conta.innerHTML=""
   basla()
+  izin();
 })
+
+
+
+/*Bildirim olarak gonderebiliyoruz */
+function izin(params) {
+    Notification.requestPermission().then(function(result) {
+        console.log("izin gedli");
+        Noti()
+      });
+}
+
+function Noti(...params) {
+    var img = '/to-do-notifications/img/icon-128.png';
+var text = params[2]+" " + params[0] + ' /'+params[1];
+var notification = new Notification('To do list', { body: text, icon: img });
+}
+
+
+
+const btn_getir=document.querySelector(".btn-getir");
+btn_getir.addEventListener('click',()=>{
+    const deger=document.querySelector("#deprem_number")
+    let calis=new deprem_filter()
+    calis.filter(deger.value);
+})
+
+class deprem_filter{
+    filter(deger){
+        console.log("class icince"+deger);
+        if(deger.value === null){ deger =20}
+        else{
+            conta.innerHTML=""
+            basla(deger)
+        }
+     
+    }
+}
